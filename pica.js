cc370b26e38884e6a851e4d99f936642b006374b
Sopolcho Kalpazanov <steven-gardiner@yahoo.com>
2014-10-22 11:53:38 -0400

    testme

---
+++ pica.js
@@ -0,0 +1,18 @@
+var pica = {};
+
+pica.mods = {};
+pica.mods.gpio = require('gpio');
+pica.mods.cp = require('child_process');
+process.on('tts', function(spec) {
+  var tts = pica.mods.cp.spawn('tts', spec.tosay);
+  tts.stdout.pipe(process.stdout);
+  tts.stderr.pipe(process.stderr);
+});
+
+pica.pin4 = pica.mods.gpio.export(4, {direction:'in', ready:function() {
+  process.emit('tts', {tosay:['ready', 'freddy']});
+}});
+
+pica.pin4.on('change', function(val) {
+  process.emit('tts', {tosay:['change', val]});
+});
