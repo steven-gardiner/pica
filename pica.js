cec779af4b05610daae6ca1a39822813d2d399be
Sopolcho Kalpazanov <steven-gardiner@yahoo.com>
2014-10-22 12:24:00 -0400

    testme

--- pica.js
+++ pica.js
@@ -6,6 +6,7 @@ pica.mods = {};
 pica.mods.gpio = require('gpio');
 pica.mods.cp = require('child_process');
 process.on('tts', function(spec) {
+  console.error("SAY: %j", spec.tosay);
   var tts = pica.mods.cp.spawn('tts', spec.tosay);
   tts.stdout.pipe(process.stdout);
   tts.stderr.pipe(process.stderr);
@@ -29,10 +30,10 @@ pica.pin22.on('change', function(val) {
 });
 
 setInterval(function() {
-  process.emit('tts', {tosay:['hear', 'ye', 4, pica.pin4.value]});
+  //process.emit('tts', {tosay:['hear', 'ye', 4, pica.pin4.value]});
 }, 5000);
 
 setInterval(function() {
   process.emit('tts', {tosay:['hear', 'ye', 17, pica.pin17.value]});
-  pica.pin17.set(!pica.pin17.value);
+  pica.pin17.set((1*!pica.pin17.value));
 }, 17500);
